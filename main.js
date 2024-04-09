19555c6d4ec1da5188c75798e48783abd9c7fe93
Pedro Henrique Furquim Gouveia <106687665+pedrohenriquefurquimgouveia@users.noreply.github.com>
2024-04-09 08:29:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const textos = document.querySelectorAll('.aba-conteudo');
 const contadores = document.querySelectorAll(".contador")
 
 
-const tempoObjetivo01 = new Date("2024.04.31T00:00:00")
-const tempoObjetivo02 = new Date("2024.04.31T00:00:00")
-const tempoObjetivo03 = new Date("2024.04.31T00:00:00")
-const tempoObjetivo04 = new Date("2024.04.31T00:00:00")
+const tempoObjetivo01 = new Date("2024-04-31T00:00:00")
+const tempoObjetivo02 = new Date("2024-04-31T00:00:00")
+const tempoObjetivo03 = new Date("2024-04-31T00:00:00")
+const tempoObjetivo04 = new Date("2024-04-31T00:00:00")
 
 const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
 
@@ -16,10 +16,10 @@ for (let i=0; i<tempos.length; i++){
 }
 
 
-for(let i=0; i<botoes.length; i++); {
+for (let i=0; i<botoes.length; i++){
     botoes[i].onclick = function (){
 
-        for(let i=0; j<botoes.length; i++); {
+        for(j=0; j<botoes.length; j++){
             botoes[j].classList.remove('ativo')
             textos[j].classList.remove('ativo')
         }
@@ -35,13 +35,13 @@ function calculaTempo (tempoObjetivo) {
     let tempoAtual = new Date()
     let tempoFinal = tempoObjetivo - tempoAtual
 
-    let segundos = Math.floor(tempo/1000);
+    let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dia = Math.floor(horas/24);
 
     segundos %= 60;
-    minuto %= 60;
+    minutos %= 60;
     horas %= 24;
 
     return dia + " Dias " + horas + " Horas " + minutos + " Minutos " + segundos + " Segundos "
